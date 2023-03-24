d4213d9728f0605117c633baea35c084cd0ce275
vvb2060 <vvb2060@gmail.com>
2023-03-24 23:36:23 +0800

    v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
     defaultConfig {
         minSdk = 28
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         externalNativeBuild {
             ndkBuild {
                 abiFilters += listOf("arm64-v8a")
